5d6f9a94b70ab2a8af6493ae346ef1e5b73571a4
JohnJayvee <johnjayveesiuagan@gmail.com>
2021-05-05 16:58:49 +0800

    Main Route

--- app/System/Routes/Web/Main.php
+++ app/System/Routes/Web/Main.php
@@ -1,7 +1,22 @@
 <?php
 
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get(
+        '/home',
+        ['uses' => 'MainController@home', 'as' => 'home']
+    );
+
+    Route::get(
+        '/admin/home',
+        ['uses' => 'MainController@home', 'as' => 'admin.home', 'middleware' => 'adminAuth']
+    );
