3e7b02f6f100233ae47a73212d97bbeecc49c83e
JohnJayvee <johnjayveesiuagan@gmail.com>
2021-05-05 16:26:36 +0800

    initial commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    // protected $namespace = 'App\\System\\Controllers';
+    protected $namespace = 'App\System\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
