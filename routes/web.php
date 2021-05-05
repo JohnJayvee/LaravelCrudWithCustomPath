0d5199f1d48bbca18b9088fb65f16b4fd46294ad
JohnJayvee <johnjayveesiuagan@gmail.com>
2021-05-05 16:13:28 +0800

    Custom path for routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+foreach(glob(app_path("System/Routes/Web/*.php")) as $file) {
+    include_once ($file);
+}
+// Route::get('/', function () {
+//     return view('welcome');
+// });
