a282c12509c1b106bf9a1d864ad4613a0f0dcf96
JohnJayvee <johnjayveesiuagan@gmail.com>
2021-05-05 16:27:20 +0800

    route for auth

---
+++ app/System/Routes/Web/Auth.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    // 'as' => "web.",
+    'namespace' => "Auth"
+    // 'domain' => env('SYSTEM_URL',''),
+], function () {
+
+    Route::get(
+        'register',
+        'RegisterController@register'
+    );
+    Route::post(
+        'register',
+        ['uses' => 'RegisterController@storeUser', 'as' => 'register']
+    );
+
+    Route::get(
+        'login',
+        ['uses' => 'LoginController@login', 'as' => 'login']
+    );
+    Route::post(
+        'login',
+        'LoginController@authenticate'
+    );
+    Route::get(
+        'logout',
+        ['uses' => 'LoginController@logout', 'as' => 'logout']
+    );
+
+    Route::get(
+        'forget-password',
+        'ForgotPasswordController@getEmail'
+    );
+    Route::post(
+        'forget-password',
+        'ForgotPasswordController@postEmail'
+    );
+
+    Route::get(
+        'reset-password/{token}',
+        'ResetPasswordController@getPassword'
+    );
+    Route::post(
+        'reset-password',
+        'ResetPasswordController@updatePassword'
+    );
+
+    Route::get(
+        '/changePassword',
+        ['uses' => 'ChangePasswordController@index', 'as' => 'changePassword']
+    );
+    Route::post(
+        '/changePassword',
+        ['uses' => 'ChangePasswordController@changePassword', 'as' => 'change.password']
+    );
+});
